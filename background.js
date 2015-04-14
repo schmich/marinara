7b3f497b8eaf9d3c720821df695bfc69f04b4fcd
Chris Schmich <schmch@gmail.com>
2015-04-14 08:09:50 -0500

    Revamp timer code. Add pause/resume plumbing. Use EventEmitter.

--- background.js
+++ background.js
@@ -5,54 +5,173 @@ var defaultSettings = {
   showNewTabNotification: true
 };
 
-function CountdownTimer(durationMin, badgeTitle, badgeColor, expireCallback) {
+function Timer(durationSec, tickSec) {
   var self = this;
-  this.updateInterval = null;
+  var state = 'stopped';
+
+  this.tickInterval = null;
   this.expireTimeout = null;
 
+  this.startTime = null;
+  this.remainingSec = null;
+
   this.start = function() {
-    if (this.updateInterval) {
-      this.stop();
-    }
-
-    var elapsedMin = 0;
-
-    this.updateInterval = setInterval(function() {
-      elapsedMin++;
-      updateBadge(Math.ceil(durationMin - elapsedMin));
-    }, 60 * 1000);
-
-    this.expireTimeout = setTimeout(function() {
-      clearInterval(self.updateInterval);
-      updateBadge(null);
-
-      self.updateInterval = null;
-      self.expireTimeout = null;
-
-      expireCallback();
-    }, durationMin * 60 * 1000);
-
-    updateBadge(Math.ceil(durationMin));
-  };
-
-  this.stop = function() {
-    if (!this.updateInterval) {
+    if (state !== 'stopped') {
       return;
     }
 
-    clearInterval(this.updateInterval);
+    this.expireTimeout = createExpireTimeout(durationSec);
+    this.tickInterval = createTickInterval(tickSec);
+
+    state = 'running';
+    this.startTime = Date.now();
+    this.emitEvent('start', [{
+      elapsed: 0,
+      remaining: durationSec
+    }]);
+  };
+
+  this.stop = function() {
+    if (state === 'stopped') {
+      return;
+    }
+
+    clearInterval(this.tickInterval);
     clearTimeout(this.expireTimeout);
-    updateBadge(null);
 
-    this.updateInterval = null;
+    this.tickInterval = null;
     this.expireTimeout = null;
+    this.startTime = null;
+    this.remainingSec = null;
+
+    state = 'stopped';
+    this.emitEvent('stop', [{}]);
   };
 
-  this.running = function() {
-    return this.updateInterval !== null;
+  this.pause = function() {
+    if (state !== 'running') {
+      return;
+    }
+
+    clearInterval(this.tickInterval);
+    clearTimeout(this.expireTimeout);
+
+    var elapsedSec = Date.now() - this.startTime;
+    this.remainingSec = durationSec - elapsedSec;
+
+    state = 'paused';
+    this.startTime = null;
+    this.emitEvent('pause', [{
+      elapsed: elapsedSec,
+      remaining: this.remainingSec
+    }]);
   };
 
-  function updateBadge(remainingMin, title) {
+  this.resume = function() {
+    if (state !== 'paused') {
+      return;
+    }
+
+    this.expireTimeout = createExpireTimeout(this.remainingSec);
+    this.tickInterval = createTickInterval(tickSec);
+
+    state = 'running';
+    this.startTime = Date.now();
+    this.emitEvent('resume', [{
+      elapsed: durationSec - this.remainingSec,
+      remaining: this.remainingSec
+    }]);
+  };
+
+  this.reset = function() {
+    this.stop();
+    this.start();
+  };
+
+  this.state = function() {
+    return state;
+  };
+
+  function createExpireTimeout(seconds) {
+    return setTimeout(function() {
+      clearInterval(self.tickInterval);
+
+      self.tickInterval = null;
+      self.expireTimeout = null;
+      self.startTime = null;
+      self.remainingSec = null;
+
+      self.emitEvent('expire', [{
+        elapsed: durationSec,
+        remaining: 0
+      }]);
+    }, seconds * 1000);
+  }
+
+  function createTickInterval(seconds) {
+    return setInterval(function() {
+      var elapsedSec = Date.now() - self.startTime;
+      var remainingSec = durationSec - elapsedSec;
+
+      self.emitEvent('tick', [{
+        elapsed: elapsedSec,
+        remaining: remainingSec
+      }]);
+    }, seconds * 1000);
+  }
+}
+
+Timer.prototype = Object.create(EventEmitter.prototype);
+
+function BadgeTimer(durationMin, badgeTitle, badgeColor) {
+  var timer = new Timer(durationMin * 60, 60);
+
+  timer.addListener('start', function(state) {
+    updateBadge(Math.round(state.remaining / 60));
+  });
+
+  timer.addListener('tick', function(state) {
+    updateBadge(Math.round(state.remaining / 60));
+  });
+
+  timer.addListener('stop', function() {
+    updateBadge(null);
+  });
+
+  timer.addListener('expire', function() {
+    updateBadge(null);
+  });
+
+  this.start = function() {
+    return timer.start();
+  };
+
+  this.stop = function() {
+    return timer.stop();
+  };
+
+  this.pause = function() {
+    return timer.pause();
+  };
+
+  this.resume = function() {
+    return timer.resume();
+  };
+
+  this.reset = function() {
+    return timer.reset();
+  };
+
+  this.state = function() {
+    return timer.state();
+  };
+
+  this.addListener = function(eventName, handler) {
+    timer.addListener(eventName, handler);
+    return this;
+  };
+
+  function updateBadge(remainingMin) {
     var text;
     if (remainingMin === null) {
       text = '';
@@ -75,13 +194,16 @@ function Pomo() {
   var breakTimer;
 
   this.cycleState = function() {
-    if (focusTimer.running()) {
+    if (focusTimer.state() !== 'stopped') {
       focusTimer.stop();
       focusNext = false;
-    } else if (breakTimer.running()) {
+    } else if (breakTimer.state() !== 'stopped') {
       breakTimer.stop();
       focusNext = true;
     } else {
+      focusTimer.stop();
+      breakTimer.stop();
+
       if (focusNext) {
         focusTimer.start();
       } else {
@@ -107,9 +229,6 @@ function Pomo() {
   };
 
   this.setSettings = function(settings, callback) {
-    focusTimer.stop();
-    breakTimer.stop();
-
     chrome.storage.sync.set(settings, function() {
       loadTimers();
       callback();
@@ -133,7 +252,12 @@ function Pomo() {
 
   function loadTimers() {
     self.getSettings(function(settings) {
-      focusTimer = new CountdownTimer(settings.focusDuration, 'Focus:', '#cc0000', function() {
+      if (focusTimer) {
+        focusTimer.stop();
+      }
+
+      focusTimer = new BadgeTimer(settings.focusDuration, 'Focus:', '#cc0000');
+      focusTimer.addListener('expire', function() {
         focusNext = false;
 
         if (settings.showDesktopNotification) {
@@ -144,8 +268,13 @@ function Pomo() {
           showExpirePage();
         }
       });
+
+      if (breakTimer) {
+        breakTimer.stop();
+      }
    
-      breakTimer = new CountdownTimer(settings.breakDuration, 'Break:', '#00cc00', function() {
+      breakTimer = new BadgeTimer(settings.breakDuration, 'Break:', '#00cc00');
+      breakTimer.addListener('expire', function() {
         focusNext = true;
 
         if (settings.showDesktopNotification) {
@@ -175,10 +304,12 @@ chrome.contextMenus.create({
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   var id = chrome.runtime.id;
+  var extensionUrl = 'chrome-extension://' + id;
+
   chrome.tabs.query({}, function(tabs) {
     var remove = [];
     for (var i = 0; i < tabs.length; ++i) {
-      if (tabs[i].url.indexOf('chrome-extension://' + id) !== -1) {
+      if (tabs[i].url.indexOf(extensionUrl) !== -1) {
         remove.push(tabs[i].id);
       }
     }
