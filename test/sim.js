fa7349e90468e0703625ee816697b96bdc55a1ee
Chris Schmich <schmch@gmail.com>
2017-10-16 01:41:05 -0500

    Add history simulation utility for testing.

---
+++ test/sim.js
@@ -0,0 +1,15 @@
+// Simulate Pomodoro history entries.
+
+async function simulate(count) {
+  function rand(lo, hi) {
+    return lo + Math.floor(Math.random() * (hi - lo + 1));
+  }
+  let origin = new Date(2015, 0, 0, 0);
+  let range = (new Date()) - +origin;
+  for (let i = 0; i < count; ++i) {
+    let timestamp = +origin + rand(0, range);
+    await controller.history.addPomodoro(25 * 60, new Date(timestamp));
+  }
+}
+
+simulate(1000).then(() => console.log('done'));
