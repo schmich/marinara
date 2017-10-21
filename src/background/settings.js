6ea088a7a067fe1ed62bda0aab06d1d1945c85e5
Chris Schmich <schmch@gmail.com>
2017-10-20 23:23:49 -0500

    Fix #92: Rename existing audio files.

--- src/background/settings.js
+++ src/background/settings.js
@@ -1,3 +1,16 @@
+// Deep clone an object. Assumes only simple types and object aggregates of simple types.
+function clone(obj) {
+  if (obj instanceof Object) {
+    let copy = {};
+    for (let prop in obj) {
+      copy[prop] = clone(obj[prop]);
+    }
+    return copy;
+  } else {
+    return obj;
+  }
+}
+
 class StorageManager extends EventEmitter
 {
   constructor(schema, storage) {
@@ -53,7 +66,7 @@ class StorageManager extends EventEmitter
 class MarinaraSchema
 {
   get version() {
-    return 2;
+    return 3;
   }
 
   get default() {
@@ -116,5 +129,44 @@ class MarinaraSchema
       },
       version: 2
     };
+  }
+
+  from2To3(v2) {
+    const fileNameMap = {
+      '/audio/battle-horn.mp3': '/audio/88736c22.mp3',
+      '/audio/bell-ring.mp3': '/audio/b10d75f2.mp3',
+      '/audio/bike-horn.mp3': '/audio/72312dd3.mp3',
+      '/audio/computer-magic.mp3': '/audio/5cf807ce.mp3',
+      '/audio/din-ding.mp3': '/audio/72cb1b7f.mp3',
+      '/audio/ding-dong.mp3': '/audio/92ff2a8a.mp3',
+      '/audio/ding.mp3': '/audio/1a5066bd.mp3',
+      '/audio/dong.mp3': '/audio/5e122cee.mp3',
+      '/audio/electronic-chime.mp3': '/audio/28d6b5be.mp3',
+      '/audio/fire-pager.mp3': '/audio/b38e515f.mp3',
+      '/audio/glass-ping.mp3': '/audio/2ed9509e.mp3',
+      '/audio/gong-1.mp3': '/audio/8bce59b5.mp3',
+      '/audio/gong-2.mp3': '/audio/85cab25d.mp3',
+      '/audio/music-box.mp3': '/audio/ebe7deb8.mp3',
+      '/audio/pin-dropping.mp3': '/audio/2e13802a.mp3',
+      '/audio/reception-bell.mp3': '/audio/54b867f9.mp3',
+      '/audio/robot-blip-1.mp3': '/audio/bd50add0.mp3',
+      '/audio/robot-blip-2.mp3': '/audio/36e93c27.mp3',
+      '/audio/ship-bell.mp3': '/audio/9404f598.mp3',
+      '/audio/toaster-oven.mp3': '/audio/a258e906.mp3',
+      '/audio/tone.mp3': '/audio/f62b45bc.mp3',
+      '/audio/train-horn.mp3': '/audio/6a215611.mp3'
+    };
+
+    let v3 = clone(v2);
+    v3.version = 3;
+
+    for (let group of [v3.focus, v3.shortBreak, v3.longBreak].map(s => s.notifications)) {
+      if (group.sound) {
+        let newName = fileNameMap[group.sound.toLowerCase()];
+        group.sound = newName || group.sound;
+      }
+    }
+
+    return v3;
   }
 }
