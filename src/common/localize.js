8e93dfdba7000fbc6d3b3312af53f3755c6e3390
Chris Schmich <schmch@gmail.com>
2017-10-25 12:04:31 -0500

    Fix #86: Use localized messages for daily distribution tooltips.

--- src/common/localize.js
+++ src/common/localize.js
@@ -1,7 +1,11 @@
 function loadLocalizedContent() {
   for (let el of document.querySelectorAll('[data-t]')) {
-    let param = el.dataset.tp;
-    el.innerText = T(el.dataset.t, param);
+    el.innerText = T(...el.dataset.t.split(','));
+  }
+
+  for (let el of document.querySelectorAll('[data-a]')) {
+    let parts = el.dataset.a.split(':');
+    el[parts[0]] = T(...parts[1].split(','));
   }
 }
 
