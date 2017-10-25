c7db7c21decdfc1254fb1ae5d8f875ccdd238551
Chris Schmich <schmch@gmail.com>
2017-10-25 08:53:25 -0500

    Allow 0-9a-z for locale selection.

--- scripts/run-localized.rb
+++ scripts/run-localized.rb
@@ -7,12 +7,11 @@ locales = Dir["#{locales_dir}/*"]
 
 puts 'Run Chrome under which locale?'
 locales.each_with_index do |locale, i|
-  puts "#{i + 1}. #{locale}"
+  puts "#{(i + 1).to_s(26)}. #{locale}"
 end
 
 print '> '
-locale = locales[gets.strip.to_i - 1]
-puts
+locale = locales[gets.strip.to_i(26) - 1]
 if locale.nil?
   puts 'Invalid choice. Exiting.'
   exit
@@ -20,6 +19,7 @@ end
 
 language = locale.gsub('_', '-')
 
+puts "Running Chrome with locale #{language}."
 `defaults write com.google.Chrome.canary AppleLanguages '("#{language}")'`
 `open -a "Google Chrome Canary"`
 `defaults delete com.google.Chrome.canary AppleLanguages`
