7f1329e35aff1451b157578756274a085c2290f5
Chris Schmich <schmch@gmail.com>
2017-10-26 04:14:38 -0500

    Improve Chrome temp session handling: use Chrome, not Chrome Canary, create temp user profile, auto-load extension.

--- scripts/run-localized.rb
+++ scripts/run-localized.rb
@@ -1,9 +1,36 @@
 require 'locales'
+require 'tmpdir'
 
 id, _ = choose_locale('Run Chrome under which locale?')
 language = id.gsub('_', '-')
 
-puts "Running Chrome with locale #{language}."
-`defaults write com.google.Chrome.canary AppleLanguages '("#{language}")'`
-`open -a "Google Chrome Canary"`
-`defaults delete com.google.Chrome.canary AppleLanguages`
+user_data_dir = Dir.mktmpdir('marinara')
+extension_dir = File.join(Dir.pwd, 'src')
+
+orig = `defaults read com.google.Chrome AppleLanguages 2>&1`
+if $?.success?
+  languages = orig.lines.slice(1, orig.lines.length - 2).map { |s| s.strip.tr(',', ' ') }
+  restore = -> { `defaults write com.google.Chrome AppleLanguages '(#{languages.join(',')})'` }
+else
+  restore = -> { `defaults delete com.google.Chrome AppleLanguages` }
+end
+
+`defaults write com.google.Chrome AppleLanguages '("#{language}")'`
+
+begin
+  args = [
+    '--new',
+    '-a',
+    'Google Chrome',
+    '--args',
+    '--no-first-run',
+    "--user-data-dir=#{user_data_dir}",
+    "--load-extension=#{extension_dir}",
+    'about:blank'
+  ]
+
+  puts "Running Chrome with locale #{language}."
+  system('open', *args)
+ensure
+  restore.call
+end
