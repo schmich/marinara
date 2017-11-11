eeddff28fb1f33c7ac68d8e7a4cbd69127fc2738
Chris Schmich <schmch@gmail.com>
2017-11-11 16:42:32 -0600

    Add desk clock timer sound.

--- src/background/sounds.js
+++ src/background/sounds.js
@@ -43,6 +43,7 @@ class Sounds
       this.timerSounds = [
         { name: T('stopwatch'), files: ['4cf03078.mp3', 'edab7b0d.mp3'] },
         { name: T('clock'), files: ['af607ff1.mp3', 'fd23aaf3.mp3'] },
+        { name: T('desk_clock'), files: ['6a981bfc.mp3', 'fd64de98.mp3'] },
         { name: T('wind_up_clock'), files: ['bc4e3db2.mp3', 'f9efd11b.mp3'] },
         { name: T('antique_clock'), files: ['875326f9.mp3', 'cba5f173.mp3'] },
         { name: T('wood_block'), files: ['ad6eac9e.mp3'] },
