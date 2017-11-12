3147de98c1e8394f1a34b7b8b9e9da20259b3335
Chris Schmich <schmch@gmail.com>
2017-11-11 18:52:26 -0600

    Add large clock timer sound.

--- src/background/sounds.js
+++ src/background/sounds.js
@@ -48,6 +48,7 @@ class Sounds
         { name: T('wind_up_clock'), files: ['bc4e3db2.mp3', 'f9efd11b.mp3'] },
         { name: T('antique_clock'), files: ['875326f9.mp3', 'cba5f173.mp3'] },
         { name: T('small_clock'), files: ['89dafd3e.mp3', '0a0ec499.mp3'] },
+        { name: T('large_clock'), files: ['2122d2a4.mp3', 'a273ba0c.mp3'] },
         { name: T('wood_block'), files: ['ad6eac9e.mp3'] },
         { name: T('metronome'), files: ['bced7c21.mp3', '9bd67f7e.mp3'] },
         { name: T('pulse'), files: ['fe5d2a62.mp3'] }
