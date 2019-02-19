8a8ffe4eaf1f9c087008057732a1182a7750603e
Chris Schmich <schmch@gmail.com>
2019-02-19 10:28:24 -0800

    Replace XMLHttpRequest-based file reading with simpler fetch implementation.

--- src/Chrome.js
+++ src/Chrome.js
@@ -215,24 +215,16 @@ class Storage
 
 class Files
 {
-  static async readFile(file, type = null) {
-    return new Promise((resolve, reject) => {
-      let req = new XMLHttpRequest();
-      if (type) {
-        req.responseType = type;
-      }
-
-      req.onload = () => resolve(req.response);
-      req.onerror = error => reject(error);
-
-      const url = chrome.runtime.getURL(file);
-      req.open('GET', url, true);
-      req.send(null);
-    });
+  static async readFile(file) {
+    let url = chrome.runtime.getURL(file);
+    let response = await fetch(url);
+    return await response.text();
   }
 
   static async readBinary(file) {
-    return this.readFile(file, 'arraybuffer');
+    let url = chrome.runtime.getURL(file);
+    let response = await fetch(url);
+    return await response.arrayBuffer();
   }
 }
 
