fe1bca9730b9104112203942b673ad1951ed575b
Chris Schmich <schmch@gmail.com>
2019-02-19 22:01:05 -0800

    Remove unnecessary Controller class. Its implementation is now just inlined in `run`.

--- src/background/main.js
+++ src/background/main.js
@@ -9,49 +9,34 @@ import { BadgeObserver, TimerSoundObserver, ExpirationSoundObserver, Notificatio
 import { ServiceBroker } from '../Service';
 import * as Alarms from './Alarms';
 
-class Controller
-{
-  static run(timer, settingsManager, settings, history) {
-    return new Controller(timer, settingsManager, settings, history);
-  }
-
-  constructor(timer, settingsManager, settings, history) {
-    this.settings = settings;
-    this.history = history;
-    this.timer = timer;
-    this.menu = createPomodoroMenu(this.timer);
-    this.timer.observe(new HistoryObserver(history));
-    this.timer.observe(new BadgeObserver());
-    this.timer.observe(new NotificationObserver(timer, settings, history));
-    this.timer.observe(new ExpirationSoundObserver(settings));
-    this.timer.observe(new TimerSoundObserver(settings));
-    this.timer.observe(new CountdownObserver(settings));
-    this.timer.observe(new MenuObserver(this.menu));
-
-    this.menu.apply();
-    settingsManager.on('change', () => this.menu.apply());
-
-    Alarms.install(timer, settingsManager);
-    chrome.browserAction.onClicked.addListener(() => this.onBrowserAction());
-  }
-
-  onBrowserAction() {
-    if (this.timer.isRunning) {
-      this.timer.pause();
-    } else if (this.timer.isPaused) {
-      this.timer.resume();
-    } else {
-      this.timer.start();
-    }
-  }
-}
-
 async function run() {
   let settingsManager = new StorageManager(new SettingsSchema(), Chrome.storage.sync);
   let settings = await PersistentSettings.create(settingsManager);
   let timer = new PomodoroTimer(settings);
   let history = new History();
-  Controller.run(timer, settingsManager, settings, history);
+
+  let menu = createPomodoroMenu(timer);
+  timer.observe(new HistoryObserver(history));
+  timer.observe(new BadgeObserver());
+  timer.observe(new NotificationObserver(timer, settings, history));
+  timer.observe(new ExpirationSoundObserver(settings));
+  timer.observe(new TimerSoundObserver(settings));
+  timer.observe(new CountdownObserver(settings));
+  timer.observe(new MenuObserver(menu));
+
+  menu.apply();
+  settingsManager.on('change', () => menu.apply());
+
+  Alarms.install(timer, settingsManager);
+  chrome.browserAction.onClicked.addListener(() => {
+    if (timer.isRunning) {
+      timer.pause();
+    } else if (timer.isPaused) {
+      timer.resume();
+    } else {
+      timer.start();
+    }
+  });
 
   ServiceBroker.register(new HistoryService(history));
   ServiceBroker.register(new SoundsService());
