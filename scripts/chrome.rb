c3f8e52ff859b5853fcbd57b389256a18aa64a0a
Chris Schmich <schmch@gmail.com>
2019-06-10 19:39:27 -0700

    Add ability to launch Chrome on Windows from scripts.

--- scripts/chrome.rb
+++ scripts/chrome.rb
@@ -1,6 +1,33 @@
 require 'tmpdir'
+require 'os'
 
 def run_chrome(language)
+  if OS.mac?
+    run_chrome_macos(language)
+  elsif OS.windows?
+    run_chrome_windows(language)
+  else
+    $stderr.puts "Launching Chrome on this OS (#{RUBY_PLATFORM}) is not implemented."
+  end
+end
+
+def run_chrome_windows(language)
+  user_data_dir = Dir.mktmpdir('marinara')
+  extension_dir = File.join(Dir.pwd, 'package')
+
+  args = [
+    '--no-first-run',
+    "--lang=#{language}",
+    "--user-data-dir=#{user_data_dir}",
+    "--load-extension=#{extension_dir}",
+    'about:blank'
+  ]
+
+  puts "Running Chrome with locale #{language}."
+  system('\Program Files (x86)\Google\Chrome\Application\chrome.exe', *args)
+end
+
+def run_chrome_macos(language)
   user_data_dir = Dir.mktmpdir('marinara')
   extension_dir = File.join(Dir.pwd, 'package')
 
