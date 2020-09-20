53eea27af23c1726ed83c5215d209236bcff24d0
Chris Schmich <schmch@gmail.com>
2020-09-19 17:05:19 -0700

    Fix whitespace preservation in vue.config.js.

--- vue.config.js
+++ vue.config.js
@@ -73,7 +73,7 @@ module.exports = {
       .use('vue-loader')
         .loader('vue-loader')
         .tap(options => {
-          options.compilerOptions.preserveWhitespace = true;
+          options.compilerOptions.whitespace = 'preserve';
           return options;
         });
   }
